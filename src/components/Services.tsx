278d3101cbc6ae5a141cbfdb07c37c70f4915043
Jacquelyn Carmichael <93010142+JVCarmich0959@users.noreply.github.com>
2025-08-27 10:56:43 -0400

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -105,7 +105,7 @@ export default function Services() {
     { 
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLineCap="round" strokeLineJoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       ),
       title: 'Quick Fixes', 
